2f094564a763841347c57e0c3e8b8e76ab01957c
hokyunR <hokyun.lee@avikus.ai>
2022-02-03 11:02:46 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# micro-frontend-links
+
+## Run
+
+```bash
+docker-compose up -d --build
+```
+
+visit `http://localhost:80`
---
+++ home/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+  
+app.listen(port);
